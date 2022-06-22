b07268522da028e7de4fd7e42c8297060677d3e8
divya pujar <divyapujar0@gmail.com>
2022-06-22 20:34:32 +0530

    added setinterval

--- GlobalObj0.js
+++ GlobalObj0.js
@@ -1,5 +1,18 @@
+const { times } = require("lodash");
 
 console.log('welcome to node.js')
- setTimeout(function(){
+ 
+setTimeout(function(){
     console.log('3sec have passed');
  },3000);
+
+var time=0;
+
+ setInterval(function(){
+    time +=2;
+    if(time>5){
+        clearInterval(time);
+    }
+    console.log(time + ' sec have passed');
+    
+ },2000);
