9eccf17d7bc29733a0ed3ba10d406e67ea4466fb
divya pujar <divyapujar0@gmail.com>
2022-06-24 13:02:55 +0530

    added comments

--- readableStream.js
+++ readableStream.js
@@ -1,7 +1,10 @@
 var fs = require('fs')
 
+// createReadStream() - open up file/stream and read data prsent in it 
+
 var myReadStream = fs.createReadStream(__dirname + '/readMe1.txt','utf8')
 
+// chunk - fragment of data that is sent to sever by the client
 myReadStream.on('data', function(chunk){
     console.log('new chunk received : ')
     console.log(chunk) 
