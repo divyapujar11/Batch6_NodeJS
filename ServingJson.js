540cae3e844be5d8a248c5a4c83231ec38348093
divya pujar <divyapujar0@gmail.com>
2022-06-24 20:16:02 +0530

    added serving json

---
+++ ServingJson.js
@@ -0,0 +1,17 @@
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function(req,res){
+    console.log('request was made: '+ req.url)
+    res.writeHead(200,{'Content-Type':'application/json'})
+    var myObj = {
+        name : 'ryu',
+        job : 'ninja',
+        age : 29
+    }
+    
+    res.end(JSON.stringify(myObj))
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('yo, listening to port 3000')
