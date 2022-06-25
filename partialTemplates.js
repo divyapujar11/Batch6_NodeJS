307fbdc9dae83aa6e415308627899a29fec96ca9
divya pujar <divyapujar0@gmail.com>
2022-06-25 17:35:59 +0530

    changes in partial templates

--- partialTemplates.js
+++ partialTemplates.js
@@ -5,11 +5,11 @@ var app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
-    res.render('index')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/contact', function(req, res){
-    res.render('contact')
+    res.sendFile(__dirname + '/contact.html')
 })
 
 app.get('/profile/:name', function(req, res){
