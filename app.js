e40e8a66208f06fa07cbfcd2d58c4c5cd3c6d510
divya pujar <divyapujar0@gmail.com>
2022-06-27 13:28:46 +0530

    created todo part2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('npm script test');
+var express = require('express')
+var todoController = require('./controllers/todoController.js')
+var app = express()
+
+//set up template engine
+app.set('view engine','ejs')
+
+//static files
+app.use('/assets',express.static('assets'))
+
+//fire controllers
+todoController(app)
+
+//listen to port
+app.listen(3000)
+console.log(' you are listening to port 3000')
